1b76612262a499955e250fb7352aa5793cc3812f
Ismaele Giuseppe Recca <igrecca27@outlook.com>
2022-01-25 23:42:04 +0000

    Tested with Hello Embree Code

--- source/HelloEmbree.cpp
+++ source/HelloEmbree.cpp
@@ -13,9 +13,9 @@ int main()
 
   float* vb = (float*) rtcSetNewGeometryBuffer(geom,
       RTC_BUFFER_TYPE_VERTEX, 0, RTC_FORMAT_FLOAT3, 3*sizeof(float), 3);
-  vb[0] = 0.f; vb[1] = 0.f; vb[2] = 0.f; // 1st vertex
-  vb[3] = 1.f; vb[4] = 0.f; vb[5] = 0.f; // 2nd vertex
-  vb[6] = 0.f; vb[7] = 1.f; vb[8] = 0.f; // 3rd vertex
+  vb[0] = 0.f; vb[1] = 0.f; vb[2] = 1.0f; // 1st vertex
+  vb[3] = 1.f; vb[4] = 0.f; vb[5] = 1.0f; // 2nd vertex
+  vb[6] = 0.f; vb[7] = 1.f; vb[8] = 1.0f; // 3rd vertex
 
   unsigned* ib = (unsigned*) rtcSetNewGeometryBuffer(geom,
       RTC_BUFFER_TYPE_INDEX, 0, RTC_FORMAT_UINT3, 3*sizeof(unsigned), 1);
