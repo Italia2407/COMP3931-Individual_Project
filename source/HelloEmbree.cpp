0851a47de17efd7e1fea8970d979019e7e04e593
Ismaele Giuseppe Recca <igrecca27@outlook.com>
2022-01-29 13:16:14 +0000

    Added Normal Direction Output

--- source/HelloEmbree.cpp
+++ source/HelloEmbree.cpp
@@ -5,6 +5,8 @@
 #include <limits>
 #include <iostream>
 
+#include <math.h>
+
 int main()
 {
   RTCDevice device = rtcNewDevice(NULL);
@@ -15,7 +17,7 @@ int main()
       RTC_BUFFER_TYPE_VERTEX, 0, RTC_FORMAT_FLOAT3, 3*sizeof(float), 3);
   vb[0] = 0.f; vb[1] = 0.f; vb[2] = 1.0f; // 1st vertex
   vb[3] = 1.f; vb[4] = 0.f; vb[5] = 1.0f; // 2nd vertex
-  vb[6] = 0.f; vb[7] = 1.f; vb[8] = 1.0f; // 3rd vertex
+  vb[6] = 0.f; vb[7] = 1.f; vb[8] = 4.0f; // 3rd vertex
 
   unsigned* ib = (unsigned*) rtcSetNewGeometryBuffer(geom,
       RTC_BUFFER_TYPE_INDEX, 0, RTC_FORMAT_UINT3, 3*sizeof(unsigned), 1);
@@ -40,6 +42,9 @@ int main()
 
   if (rayhit.hit.geomID != RTC_INVALID_GEOMETRY_ID) {
     std::cout << "Intersection at t = " << rayhit.ray.tfar << std::endl;
+
+    float normMag = sqrt(pow(rayhit.hit.Ng_x, 2.0f) + pow(rayhit.hit.Ng_y, 2.0f) + pow(rayhit.hit.Ng_z, 2.0f));
+    std::cout << "Normal Direction = x: " << rayhit.hit.Ng_x / normMag << ", y: " << rayhit.hit.Ng_y / normMag << ", z: " << rayhit.hit.Ng_z / normMag << std::endl;
   } else {
     std::cout << "No Intersection" << std::endl;
   }
